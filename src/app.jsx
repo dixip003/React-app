f1df4e5663f7dc8d12e1cf9b52aedcc64298e9d1
Pankaj Dixit <Pankaj.Dixit@acuitykp.com>
2020-08-26 08:12:08 +0530

    added bootstrap to project

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,21 @@ import React, {Component, Fragment} from 'react';
 import Button from "./button/button";
 import styles from './styles/main.scss';
 import FancyButton from "./fancy-button/fancy-button";
+// Import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const content = 'Hello world!';
 
 const App = () => (
   <Fragment>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">bbb</div>
+        <div className="col-6">abc</div>
+      </div>
+    </div>
     <div className={styles["resizeable-both"]}>{content}</div>
     <Button label="Regular Button"/>
     <FancyButton label="Fancy Button"/>
